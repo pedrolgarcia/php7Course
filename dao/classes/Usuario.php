7bf33bb818d8bed5ddc9c76190299907ef2a6c87
Pedro Lopes <pedrolopes.info@gmail.com>
2018-05-01 15:02:46 -0300

    Novas funções DAO

--- dao/classes/Usuario.php
+++ dao/classes/Usuario.php
@@ -6,6 +6,11 @@
         private $dessenha;
         private $dtcadastro;
 
+        public function __construct($login = "", $password = ""){
+            $this->setDeslogin($login);
+            $this->setDessenha($password);
+        }
+
         public function getIdusuario(){
             return $this->idusuario;
         }
@@ -43,14 +48,65 @@
             $res = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>$id));
 
             if(count($res) > 0){
-                $row = $res[0];
-
-                $this->setIdusuario($row['idusuario']);
-                $this->setDeslogin($row['deslogin']);
-                $this->setDessenha($row['dessenha']);
-                $this->setDtcadastro(new DateTime($row['dtcadastro']));
+                $this->setData($res[0]);
             }
             
+        }
+
+        public static function getList(){
+            $sql = new SQL();
+            return $sql->select("SELECT * FROM tb_usuarios"); 
+        }
+
+        public static function search($login){
+            $sql = new SQL();
+            return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :LOGIN", array(":LOGIN"=>"%".$login."%"));
+        } //Retorna todos os usuários que contém (%%) a string no nome
+
+        public function login($login, $password){
+            $sql = new SQL();
+            $res = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+                ":LOGIN"=>$login,
+                ":PASSWORD"=>$password
+            ));
+
+            if(count($res) > 0){
+                $this->setData($res[0]);
+            } else {
+                throw new Exception("Login e/ou senha inválidos.");
+                
+            }
+        }
+
+        public function setData($data){
+            $this->setIdusuario($data['idusuario']);
+            $this->setDeslogin($data['deslogin']);
+            $this->setDessenha($data['dessenha']);
+            $this->setDtcadastro(new DateTime($data['dtcadastro']));
+        }
+
+        public function insert(){
+            $sql = new SQL();
+            $res = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+                ":LOGIN"=>$this->getDeslogin(),
+                ":PASSWORD"=>$this->getDessenha()
+            ));
+            if(count($res) > 0){
+                $this->setData($res[0]);
+            }
+        }
+
+        public function update($login, $password){
+            $this->setDeslogin($login);
+            $this->setDessenha($password);
+
+            $sql = new SQL();
+            $res = $sql->query("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+                ":ID"=>$this->getIdusuario(),
+                ":LOGIN"=>$this->getDeslogin(),
+                ":PASSWORD"=>$this->getDessenha()
+            ));
+
         }
 
         public function __toString(){
--- dao/index.php
+++ dao/index.php
@@ -2,15 +2,33 @@
     
     require_once("config.php");
 
-    $user = new Usuario();
-
+    //Carrega um usuário buscando pelo id
+    /*$user = new Usuario();
     $user->loadById(4);
+    echo $user;*/
 
+    //Carrega uma lista de todos os usuários
+    /*$list = Usuario::getList(); //Método estático
+    echo json_encode($list);*/
+
+    //Carrega uma lista de usuários buscando pelo login
+    /*$listL = Usuario::search("jo"); Todos os usuários que contém "jo" no nome
+    echo json_encode($listL);*/
+
+    //Carrega um usuário usando um login e uma senha 
+    /*$user = new Usuario();
+    $user->login("pedro", "123456");
+    echo $user;*/
+
+    //Insere um novo usuário
+    /*$aluno = new Usuario("aluno", "@lun0");
+    $aluno->insert();
+    echo $aluno;*/
+
+    //Alterando um usuário
+    $user = new Usuario();
+    $user->loadById(8);
+    $user->update("professor", "!@#$%");
     echo $user;
-
-    /*$sql = new SQL();
-    $res = $sql->select("SELECT * FROM tb_usuarios");
-    //var_dump($res);
-    echo json_encode($res);*/
 
 ?>
